cdf6a051105f7dae2e000a3cbc6dfe1cb9f86f98
Drynxx <tmatti3456@gmail.com>
2024-01-16 00:02:03 +0200

    first commit

---
+++ src/_root/pages/AllUsers.tsx
@@ -0,0 +1,43 @@
+import Loader from "@/components/shared/Loader";
+import UserCard from "@/components/shared/UserCard";
+import { useToast } from "@/components/ui/use-toast";
+import { useGetPosts, useGetUsers } from "@/lib/react-query/queriesAndMutations";
+import {useInView} from 'react-intersection-observer'
+import { useEffect, useState } from "react";
+import useDebounce from "@/hooks/useDebounce";
+
+
+const AllUsers = () => {
+  const { toast } = useToast();
+  const [searchValue, setSearchValue] = useState('')
+  const debouncedValue = useDebounce(searchValue, 500);
+
+  const { data: creators, isLoading, isError: isErrorCreators } = useGetUsers();
+
+  if (isErrorCreators) {
+    toast({ title: "Something went wrong." });
+    
+    return;
+  }
+
+  return (
+    <div className="common-container">
+      <div className="user-container">
+        <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+        {isLoading && !creators ? (
+          <Loader />
+        ) : (
+          <ul className="user-grid">
+            {creators?.documents.map((creator) => (
+              <li key={creator?.$id} className="flex-1 min-w-[200px] w-full  ">
+                <UserCard user={creator} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
